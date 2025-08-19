68006a6e44fdf7e98d926dbc02d40ab32f50f2c2
PhilipJPrice <philip.price.1994@gmail.com>
2025-08-19 16:37:22 -0700

    Changed font weights, margins, and padding to look more like picture

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ export function App() {
 				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
 				Coded by <a href="#">Philip Price</a>.
 			</div>
-			<div class="flex flex-col mx-auto bg-white justify-center rounded-xl shadow-lg w-72 h-auto p-4 text-center">
-				<img class="rounded-xl" src={qrcode} alt="QR Code" />
-				<h1 class="text-xl m-2">Improve your front-end skills by building projects</h1>
-				<p class="text-base text-slate-500">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.</p>
+			<div class="flex flex-col mx-auto bg-white justify-center rounded-xl shadow-lg w-72 h-auto p-3 text-center">
+				<img class="rounded-xl mb-2" src={qrcode} alt="QR Code" />
+				<h1 class="text-xl m-2 font-medium">Improve your front-end skills by building projects</h1>
+				<p class="text-base text-slate-500 font-light mb-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.</p>
 			</div>
 		</div>
 	)
