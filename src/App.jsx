4e0fa0ac0a134d917e2e6c6d3b584cd0e6f263bd
PhilipJPrice <philip.price.1994@gmail.com>
2025-09-02 12:13:37 -0700

    fix: changed class on jsx elements to classname

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import qrcode from './images/image-qr-code.png'
 
 export function App() {
 	return (
-		<div class="h-screen flex flex-col justify-center items-center">
-			<div class="attribution">
+		<div className="h-screen flex flex-col justify-center items-center">
+			<div className="attribution">
 				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
 				Coded by <a href="#">Philip Price</a>.
 			</div>
-			<div class="flex flex-col mx-auto bg-white justify-center rounded-xl shadow-lg w-72 h-auto p-3 text-center">
-				<img class="rounded-xl mb-2" src={qrcode} alt="QR Code" />
-				<h1 class="text-xl m-2 font-medium">Improve your front-end skills by building projects</h1>
-				<p class="text-base text-slate-500 font-light mb-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.</p>
+			<div className="flex flex-col mx-auto bg-white justify-center rounded-xl shadow-lg w-72 h-auto p-3 text-center">
+				<img className="rounded-xl mb-2" src={qrcode} alt="QR Code" />
+				<h1 className="text-xl m-2 font-medium">Improve your front-end skills by building projects</h1>
+				<p className="text-base text-slate-500 font-light mb-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.</p>
 			</div>
 		</div>
 	)
